07baf9d3f1cb4424674afd055de3a649a818b4c6
hungnq98 <hungnq10@vnpt.vn>
2021-12-02 11:31:33 +0700

    deployment config

--- connection.js
+++ connection.js
@@ -3,9 +3,9 @@ const os = require("os");
 
 
 const connection = mysql.createConnection({
-  host: os.platform() == "win32" ? "localhost" : "203.171.21.65",
+  host: "localhost",
   user: "root",
-  password: os.platform() == "win32" ? "1234" : "Passw@rd123",
+  password: "1234",
   port: os.platform() == "win32" ? 3306 : 3307,
   database: "hstudy",
   connectTimeout: 60000,
